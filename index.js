1e78d1a1ecdbac4d68dde18af4f95a62503966d1
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-11-21 21:45:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function objectToCSS(styles, nested_lv = 0) {
     if(styles[propety].toString() == "[object Object]") {
       styleList.push(" ".repeat(nested_lv * 4) + propety + " {\n" + objectToCSS(styles[propety], nested_lv + 1) + "\n" + " ".repeat(nested_lv * 4) + "}");
     } else {
+      if(typeof styles[propety] == "number" && styles[propety]) styles[propety] += "px";
       styleList.push(" ".repeat(nested_lv * 4) + propety.toSnakeCase() + ": " + styles[propety] + ";");
     }
   }
